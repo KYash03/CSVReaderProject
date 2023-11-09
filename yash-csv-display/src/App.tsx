b1bda2b1c224abc2e9b7a0d36188299abc577792
prathamkshah13 <prathamkshah130503@gmail.com>
2023-11-09 09:30:03 +0530

    Additions

--- yash-csv-display/src/App.tsx
+++ yash-csv-display/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import CSVTable from "./CSVTable";
 
 const App: React.FC = () => {
-  const [data, setData] = useState<string[][]>([]);
+  const [data, setData] = useState<{ session: string; data: string[][] }[]>([]);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -12,16 +12,32 @@ const App: React.FC = () => {
     const reader = new FileReader();
     reader.onload = (event) => {
       const csv = event.target?.result as string;
-      const parsedData = csv.split("\n").map((row) => row.split("\t"));
-      setData(parsedData);
+      const parsedData = csv.split("\n").map((row) => row.split(","));
+      setData(groupDataBySession(parsedData));
     };
     reader.readAsText(file);
+  };
+
+  const groupDataBySession = (data: string[][]): { session: string; data: string[][] }[] => {
+    const sessions: { [key: string]: string[][] } = {};
+
+    for (let i = 1; i < data.length; i++) {
+      const session = data[i][6]; // Assuming the "Session" column is at index 6
+      if (!sessions[session]) {
+        sessions[session] = [];
+      }
+      sessions[session].push(data[i]);
+    }
+
+    return Object.keys(sessions).map((session) => ({ session, data: sessions[session] }));
   };
 
   return (
     <div className="App">
       <input type="file" onChange={handleFileChange} />
-      <CSVTable data={data} />
+      {data.map((sessionData, index) => (
+        <CSVTable key={index} data={sessionData.data} session={sessionData.session} />
+      ))}
     </div>
   );
 };
--- yash-csv-display/src/CSVTable.tsx
+++ yash-csv-display/src/CSVTable.tsx
@@ -2,30 +2,45 @@ import React from "react";
 
 interface Props {
   data: string[][];
+  session: string;
 }
 
-const CSVTable: React.FC<Props> = ({ data }) => {
+const CSVTable: React.FC<Props> = ({ data, session }) => {
   if (data.length === 0) return null;
 
+  // Define the column indices for Course, Grade, Letter, Standing, and Class Average
+  const courseIndex = 0;
+  const gradeIndex = 2; // Column "Grade"
+  const letterIndex = 3;
+  const standingIndex = 4; // Column "Standing"
+  const classAvgIndex = 10; // Column "Credits Earned"
+
   return (
-    <table>
-      <thead>
-        <tr>
-          {data[0].map((header, idx) => (
-            <th key={idx}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.slice(1).map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((cell, cellIndex) => (
-              <td key={cellIndex}>{cell}</td>
-            ))}
+    <div>
+      <h2 className="SessionHeader">Session - {session}</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Course</th>
+            <th>Grade</th>
+            <th>Letter</th>
+            <th>Standing</th>
+            <th>Class Avg</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              <td>{row[courseIndex]}</td> {/* Course */}
+              <td>{row[gradeIndex]}</td> {/* Grade */}
+              <td>{row[letterIndex]}</td> {/* Letter */}
+              <td>{row[standingIndex]}</td> {/* Standing */}
+              <td>{row[classAvgIndex]}</td> {/* Class Avg */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
